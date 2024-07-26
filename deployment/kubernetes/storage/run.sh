7091e17f3cf68b560d01186ca478a3e1889fd6f4
Espoir Murhabazi <EspoirMur@users.noreply.github.com>
2024-07-26 09:50:53 +0100

    add deployment stuff

---
+++ deployment/kubernetes/storage/run.sh
@@ -0,0 +1,9 @@
+
+
+echo "$ACCESS_KEY_ID:$ORACLE_CLOUD_SECRET" > passwd && chmod 600 passwd
+URL=https://$TENANT_ID.compat.objectstorage.$REGION.oraclecloud.com 
+s3fs -f -d $OCI_BUCKET $MOUNT_POINT -o endpoint=$REGION -o passwd_file=passwd -o url=$URL -o nomultipart -o use_path_request_style 
+
+# to run the container with this scrip, use the following command
+
+# docker run --privileged -d --name s3fs-test -e ORACLE_CLOUD_SECRET=secret -e OCI_BUKCET=bio-gpt-embedding  -e REGION=uk-london-1 -e TENANT_ID=tenant s3fs
