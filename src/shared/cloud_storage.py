c17f1b30d91e02117643c159797e9fc69802bf2f
Espoir Murhabazi <EspoirMur@users.noreply.github.com>
2025-01-16 20:47:11 +0000

    add notes

--- src/shared/cloud_storage.py
+++ src/shared/cloud_storage.py
@@ -1,9 +1,19 @@
 import os
+from datetime import datetime
 from pathlib import Path
+from tempfile import NamedTemporaryFile
 
 import b2sdk.v2 as back_blaze
+import numpy as np
+import pandas as pd
 from b2sdk._internal.transfer.inbound.downloaded_file import DownloadedFile
 from dotenv import load_dotenv
+
+from src.shared.logger import setup_logger
+
+logger = setup_logger("data_puller")
+
+BUCKET_NAME = "congonews-clusters"
 
 
 class BackBlazeCloudStorage:
@@ -54,3 +64,45 @@ class BackBlazeCloudStorage:
 		"""download the file from the bucket with the given name"""
 		bucket = self.get_bucket(bucket_name)
 		return bucket.download_file_by_name(file_name)
+
+	def save_df_to_blackbaze_bucket(
+		self, data: pd.DataFrame, bucket_name: str = BUCKET_NAME, **kwargs
+	) -> None:
+		"""Save a dataframe to a cloud bucket."""
+		today = datetime.now().strftime("%Y-%m-%d")
+		date = kwargs.get("date", today)
+		file_name = f"news-clusters-{today}-to-{date}.csv"
+		with NamedTemporaryFile(delete=True, suffix=".csv") as temp_file:
+			data.to_csv(temp_file, sep="|")
+			self.upload_file(
+				bucket_name=bucket_name,
+				file_path=temp_file.name,
+				file_name=file_name,
+				metadata=kwargs,
+			)
+			logger.info(f"Saved {date} news to the cloud bucket")
+			return file_name
+
+	def download_file_as_numpy_array(
+		self, bucket_name: str, file_name: str
+	) -> np.array:
+		"""Given the filename, download the file and return it as a numpy array"""
+		documents = self.download_by_name(bucket_name=bucket_name, file_name=file_name)
+		with NamedTemporaryFile(delete=True, suffix=".csv") as temp_file:
+			documents.save_to(temp_file.name)
+			csv_data = np.loadtxt(
+				temp_file.name,
+				delimiter="|",
+				skiprows=1,
+				dtype=[("index", "i8"), ("content", "U10000")],
+				encoding="utf-8",
+			)
+			csv_data = np.array(csv_data.tolist())
+		return csv_data
+
+	def download_npy_file(self, bucket_name: str, file_name: str) -> np.array:
+		"""Given the filename, download the file and return it as a numpy array"""
+		documents = self.download_by_name(bucket_name=bucket_name, file_name=file_name)
+		with NamedTemporaryFile(delete=True, suffix=".npy") as temp_file:
+			documents.save_to(temp_file.name)
+			return np.load(temp_file.name)
