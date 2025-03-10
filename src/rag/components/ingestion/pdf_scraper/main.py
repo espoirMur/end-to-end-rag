bf82ba09f0aec68585a25d354f82af6bee87284c
Espoir Murhabazi <EspoirMur@users.noreply.github.com>
2025-03-10 21:35:28 +0000

    fix the document parser script

--- src/rag/components/ingestion/pdf_scraper/main.py
+++ src/rag/components/ingestion/pdf_scraper/main.py
@@ -2,7 +2,12 @@ import os
 import time
 from pathlib import Path
 
+from sentence_transformers import SentenceTransformer
+
 from src.rag.components.ingestion.pdf_scraper.parser import MyDocumentParser
+from src.shared.logger import setup_logger
+
+logger = setup_logger("pdf_parser")
 
 # Parse all documents
 if __name__ == "__main__":
@@ -10,14 +15,16 @@ if __name__ == "__main__":
 	books_path = documents_path.joinpath("Documents")
 	file_names = books_path.glob("**/*.pdf")
 	output_path = Path.cwd().joinpath("datasets", "parsed_documents")
+	embedding_model = "intfloat/multilingual-e5-large"
+	model = SentenceTransformer(embedding_model)
+	model.to("cpu")
+	logger.info(f"Using embedding model: {embedding_model}")
 	output_path.mkdir(parents=True, exist_ok=True)
 	max_workers = os.cpu_count()
-	my_parser = MyDocumentParser(output_path=output_path)
+	my_parser = MyDocumentParser(output_path=output_path, embedding_model=model)
 	start_time = time.time()
-	parsed_documents = my_parser.parse_documents_parallel(
-		file_names, max_workers=max_workers
-	)
-	my_parser.save_parsed_documents(parsed_documents)
+	file_names = list(file_names)
+	my_parser.parse_documents_parallel(file_names, max_workers=max_workers)
 	end_time = time.time()
 
 	print(f"Time taken to parse documents: {end_time - start_time} seconds")
--- src/rag/components/ingestion/pdf_scraper/parser.py
+++ src/rag/components/ingestion/pdf_scraper/parser.py
@@ -4,17 +4,23 @@ from typing import Dict, List, Optional
 
 from openparse import DocumentParser
 from openparse.schemas import ParsedDocument
+from sentence_transformers import SentenceTransformer
 from tqdm import tqdm
 
 from src.shared.logger import setup_logger
 
-logger = setup_logger("data_puller")
+logger = setup_logger("pdf_parser")
 
 
 class MyDocumentParser:
 	"""This is my custom document parser that will use pypdf parser to extract text from PDF files."""
 
-	def __init__(self, output_path: Path, document_parser_kwargs: Dict = {}):
+	def __init__(
+		self,
+		output_path: Path,
+		document_parser_kwargs: Dict = {},
+		embedding_model: SentenceTransformer = None,
+	):
 		"""
 		Initialize the MyDocumentParser.
 
@@ -25,59 +31,84 @@ class MyDocumentParser:
 		self.document_parser_kwargs = document_parser_kwargs
 		self.parser = DocumentParser(**document_parser_kwargs)
 		self.output_path = output_path
+		self.embedding_model = embedding_model
+
+	def compute_embeddings(self, document: ParsedDocument) -> ParsedDocument:
+		"""
+		Computes embeddings for the nodes in the given ParsedDocument using the specified embedding model.
+
+		Args:
+		    document (ParsedDocument): The document containing nodes for which embeddings need to be computed.
+
+		Returns:
+		    ParsedDocument: The document with computed embeddings for each node.
+
+		Note:
+		    If the embedding model is not set (None), the document is returned without any modifications.
+		"""
+		if self.embedding_model is None:
+			return document
+
+		nodes = document.nodes
+		node_embedding = self.embedding_model.encode(
+			[node.text for node in nodes],
+			convert_to_tensor=False,
+			show_progress_bar=True,
+		)
+		for node, embedding in zip(nodes, node_embedding):
+			node.embedding = embedding.tolist()
+		return document
 
 	def parse_document(self, document_path: Path) -> Optional[ParsedDocument]:
 		"""
-		Parse a single document.
+		Parse a single document, compute the embedding and save it to a path.
 
 		Args:
 		    document_path (Path): The path to the document to be parsed.
-
-		Returns:
-		    ParsedDocument: The parsed document.
 		"""
 		try:
 			parsed_basic_doc = self.parser.parse(document_path)
 			logger.info(f"Successfully parsed {document_path}")
-			return parsed_basic_doc
+			parsed_doc_with_embeddings = self.compute_embeddings(parsed_basic_doc)
+			self.save_parsed_document(parsed_doc_with_embeddings)
+			return parsed_doc_with_embeddings
 		except Exception as e:
 			logger.error(f"Error parsing {document_path}: {e}")
 			return None
 
 	def parse_documents_parallel(
-		self, file_names, max_workers=4
+		self, file_names: List[Path], max_workers: int = 4
 	) -> List[Optional[ParsedDocument]]:
 		"""
 		Parse multiple documents in parallel.
 
 		Args:
-		    file_names (iterable): An iterable of file paths to be parsed.
+		    file_names (List[Path]): A list of file paths to be parsed.
 		    max_workers (int): The maximum number of worker processes to use.
 
 		Returns:
-		    List[ParsedDocument]: A list of parsed documents.
+		    List[Optional[ParsedDocument]]: A list of parsed documents.
 		"""
-		results = []
 		with ProcessPoolExecutor(max_workers=max_workers) as executor:
-			# Use tqdm to show progress
-			futures = list(
-				tqdm(executor.map(self.parse_document, file_names), total=700)
+			results = list(
+				tqdm(
+					executor.map(self.parse_document, file_names), total=len(file_names)
+				)
 			)
-
-			for future in futures:
-				results.append(future)
 		return results
 
-	def save_parsed_documents(self, parsed_documents: List[ParsedDocument]):
+	def save_parsed_document(self, parsed_document: ParsedDocument):
 		"""
 		Save parsed documents to the output path.
 
 		Args:
-		    parsed_documents (List[ParsedDocument]): A list of parsed documents to be saved.
+		    parsed_document (ParsedDocument): The parsed document to be saved.
 		"""
-		for parsed_document in parsed_documents:
-			if parsed_document is None:
-				continue
-			output_file = self.output_path.joinpath(f"{parsed_document.filename}.json")
-			with open(output_file, "w") as f:
-				f.write(parsed_document.model_dump_json())
+		if parsed_document is None:
+			logger.warning("Skipping None document")
+			return
+
+		output_file = self.output_path.joinpath(f"{parsed_document.filename}.json")
+		with open(output_file, "w") as f:
+			f.write(parsed_document.model_dump_json())
+		logger.info(f"Saved parsed document to {output_file}")
